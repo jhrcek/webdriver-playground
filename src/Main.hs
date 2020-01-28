94823c4915eaa51509c517d68a9c243074d372bd
Jan Hrček <jhrcek@globalwebindex.com>
2020-01-28 14:16:10 +0100

    Automate selenium server start/stop

--- src/Main.hs
+++ src/Main.hs
@@ -1,61 +1,109 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
 
 module Main where
 
-import qualified Control.Foldl  as Fold
-import qualified Data.Text      as Text
-import qualified Data.Text.Read as TR
+import           Control.Concurrent (forkIO, killThread)
+import           Control.Exception  (bracket)
+import qualified Control.Foldl      as Fold
+import qualified Data.Text          as Text
+import qualified Data.Text.Read     as TR
+import qualified Scenario
 import           Turtle
 
-import qualified Scenario
-
-
+-- TODO automate selenium server download https://selenium.dev/downloads/ - https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar
 main :: IO ()
 main = do
-  Scenario.run
-  
+  setupChromedriver
+  withSeleniumServer Scenario.run
+
+withSeleniumServer :: IO () -> IO ()
+withSeleniumServer action = do
+  bracket
+    (forkIO runSeleniumServer)
+    killThread
+    (const $ do
+       sleep 2
+       action
+       sleep 1)
+  where
+    runSeleniumServer :: IO ()
+    runSeleniumServer =
+      procs "java" ["-jar", "selenium-server-standalone-3.141.59.jar"] empty
+
 setupChromedriver :: IO ()
 setupChromedriver = do
-  eMajorVersion <- getGoogleChromeVersionMajor
-  case eMajorVersion of 
-    Left err -> die err
-    Right majorVersion -> downloadChromedriver majorVersion
-    
+  res1 <- getGoogleChromeVersionMajor
+  case res1 of
+    Version browserVersion -> do
+      res2 <- getChromedriverVersionMajor
+      case res2 of
+        Version chromedriverVersion ->
+          when (browserVersion /= chromedriverVersion) $
+            downloadChromedriver browserVersion
+        NotAvailable -> downloadChromedriver browserVersion
+        _ -> die $ Text.pack $ show res2
+    _ -> die $ Text.pack $ show res1
+
+data CommantResult
+  = NotAvailable
+  | VersionParseFailure Text
+  | NoOutput
+  | Version Int
+  deriving (Show)
 
 {- "Google Chrome 79.0.3945.130" -> Right 79-}
-getGoogleChromeVersionMajor :: IO (Either Text Int)
+getGoogleChromeVersionMajor :: IO CommantResult
 getGoogleChromeVersionMajor = do
   mChromeBinary <- which "google-chrome"
   case mChromeBinary of
-    Nothing -> throw "`google-chrome` binary is not on PATH"
-    Just chromeBinary -> do
+    Nothing -> pure NotAvailable
+    Just _chromeBinary -> do
       mStdout <- fold (inshell "google-chrome --version" empty) Fold.head
       case fmap lineToText mStdout of
-        Nothing -> throw "`google-chrome --version` didn't produce output"
+        Nothing -> pure NoOutput
         Just out ->
           case Text.words out of
             (_:_:versionText:_) ->
               case TR.decimal versionText of
-                Left err                  -> throw $ Text.pack err
-                Right (numericVersion, _) -> pure $ Right numericVersion
-            _ ->
-              throw $ "`google-chrome --version` had unexpected output: " <> out
-  where
-    throw = pure . Left
+                Left er -> pure $ VersionParseFailure $ Text.pack er
+                Right (numericVersion, _) -> pure $ Version numericVersion
+            _ -> pure $ VersionParseFailure out
+
+getChromedriverVersionMajor :: IO CommantResult
+getChromedriverVersionMajor = do
+  exists <- testfile "chromedriver"
+  if exists
+    then do
+      mStdout <- fold (inshell "./chromedriver --version" empty) Fold.head
+      case fmap lineToText mStdout of
+        Nothing -> pure NoOutput
+        Just out ->
+          case Text.words out of
+            (_:versionText:_) ->
+              case TR.decimal versionText of
+                Left er -> pure $ VersionParseFailure $ Text.pack er
+                Right (numericVersion, _) -> pure $ Version numericVersion
+            _ -> pure $ VersionParseFailure out
+    else pure NotAvailable
 
 downloadChromedriver :: Int -> IO ()
 downloadChromedriver numericVersion = do
-  let releaseUrl = Text.pack $ "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_" <> show numericVersion
+  let releaseUrl =
+        Text.pack $
+        "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_" <>
+        show numericVersion
   mExactVersion <- fold (inproc "curl" [releaseUrl] empty) Fold.head
   case mExactVersion of
-    Nothing -> die $ "Failed to retrieve exact version of chromedriver from " <> releaseUrl
+    Nothing ->
+      die $
+      "Failed to retrieve exact version of chromedriver from " <> releaseUrl
     Just exactVersion -> do
       let chromedriverZip = "chromedriver_linux64.zip"
-          chromedriverUrl = "https://chromedriver.storage.googleapis.com/" <> lineToText exactVersion <> "/" <> chromedriverZip
+          chromedriverUrl =
+            "https://chromedriver.storage.googleapis.com/" <>
+            lineToText exactVersion <> "/" <> chromedriverZip
       sh $ do
         procs "wget" [chromedriverUrl] empty
         procs "unzip" [chromedriverZip] empty
         rm $ fromText chromedriverZip
-
--- TODO automate selenium server download https://selenium.dev/downloads/
--- TODO autoamte selenium server start/stop
--- src/Scenario.hs
+++ src/Scenario.hs
@@ -5,10 +5,10 @@ module Scenario where
 import           Test.WebDriver
 
 chromeConfig :: WDConfig
-chromeConfig = useBrowser chrome (defaultConfig {wdHost = "192.168.122.1"})
+chromeConfig = useBrowser chrome defaultConfig
 
 run :: IO ()
-run =
+run = do
   runSession chromeConfig . finallyClose $ do
     openPage "http://google.com"
     searchInput <- findElem (ByCSS "input[type='text']")
