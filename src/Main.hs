3d0237e3eb93963df35f3bded4100b67ce1946e7
Jan Hrček <honza.hrk@gmail.com>
2020-06-06 11:18:28 +0200

    Bump to lts-15.15, automate selenium server download

--- src/Main.hs
+++ src/Main.hs
@@ -1,31 +1,42 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main where
 
-import           Control.Concurrent (forkIO, killThread)
-import           Control.Exception  (bracket)
-import qualified Control.Foldl      as Fold
-import qualified Data.Text          as Text
-import qualified Data.Text.Read     as TR
+import Control.Concurrent (forkIO, killThread)
+import Control.Exception (bracket)
+import qualified Control.Foldl as Fold
+import qualified Data.Text as Text
+import qualified Data.Text.Read as TR
 import qualified Scenario
-import           Turtle
+import Turtle
 
--- TODO automate selenium server download https://selenium.dev/downloads/ - https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar
 main :: IO ()
 main = do
+  downloadSeleniumServer
   setupChromedriver
   withSeleniumServer Scenario.run
+
+downloadSeleniumServer :: IO ()
+downloadSeleniumServer = do
+  exists <- testfile (fromText seleniumServerJar)
+  unless exists $ do
+    echo "Downloading selenium server"
+    procs "wget" ["https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar"] empty
+
+seleniumServerJar :: Text
+seleniumServerJar = "selenium-server-standalone-3.141.59.jar"
 
 withSeleniumServer :: IO () -> IO ()
 withSeleniumServer action = do
   bracket
     (forkIO runSeleniumServer)
     killThread
-    (const $ do
-       sleep 2
-       action
-       sleep 1)
+    ( const $ do
+        sleep 2
+        action
+        sleep 1
+    )
   where
     runSeleniumServer :: IO ()
     runSeleniumServer =
@@ -45,7 +56,7 @@ setupChromedriver = do
         _ -> die $ Text.pack $ show res2
     _ -> die $ Text.pack $ show res1
 
-data CommantResult
+data CommandResult
   = NotAvailable
   | VersionParseFailure Text
   | NoOutput
@@ -53,7 +64,7 @@ data CommantResult
   deriving (Show)
 
 {- "Google Chrome 79.0.3945.130" -> Right 79-}
-getGoogleChromeVersionMajor :: IO CommantResult
+getGoogleChromeVersionMajor :: IO CommandResult
 getGoogleChromeVersionMajor = do
   mChromeBinary <- which "google-chrome"
   case mChromeBinary of
@@ -64,13 +75,13 @@ getGoogleChromeVersionMajor = do
         Nothing -> pure NoOutput
         Just out ->
           case Text.words out of
-            (_:_:versionText:_) ->
+            (_ : _ : versionText : _) ->
               case TR.decimal versionText of
                 Left er -> pure $ VersionParseFailure $ Text.pack er
                 Right (numericVersion, _) -> pure $ Version numericVersion
             _ -> pure $ VersionParseFailure out
 
-getChromedriverVersionMajor :: IO CommantResult
+getChromedriverVersionMajor :: IO CommandResult
 getChromedriverVersionMajor = do
   exists <- testfile "chromedriver"
   if exists
@@ -80,7 +91,7 @@ getChromedriverVersionMajor = do
         Nothing -> pure NoOutput
         Just out ->
           case Text.words out of
-            (_:versionText:_) ->
+            (_ : versionText : _) ->
               case TR.decimal versionText of
                 Left er -> pure $ VersionParseFailure $ Text.pack er
                 Right (numericVersion, _) -> pure $ Version numericVersion
@@ -91,18 +102,20 @@ downloadChromedriver :: Int -> IO ()
 downloadChromedriver numericVersion = do
   let releaseUrl =
         Text.pack $
-        "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_" <>
-        show numericVersion
+          "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_"
+            <> show numericVersion
   mExactVersion <- fold (inproc "curl" [releaseUrl] empty) Fold.head
   case mExactVersion of
     Nothing ->
       die $
-      "Failed to retrieve exact version of chromedriver from " <> releaseUrl
+        "Failed to retrieve exact version of chromedriver from " <> releaseUrl
     Just exactVersion -> do
       let chromedriverZip = "chromedriver_linux64.zip"
           chromedriverUrl =
-            "https://chromedriver.storage.googleapis.com/" <>
-            lineToText exactVersion <> "/" <> chromedriverZip
+            "https://chromedriver.storage.googleapis.com/"
+              <> lineToText exactVersion
+              <> "/"
+              <> chromedriverZip
       sh $ do
         procs "wget" [chromedriverUrl] empty
         procs "unzip" [chromedriverZip] empty
